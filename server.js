169cfab21ccbff499bd3876a55feac100e54b858
Dave Furfero <furftastic@yahoo.com>
2010-10-24 23:29:05 -0400

    cleanup

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require.paths.push(__dirname + '/lib');
 require.paths.push(__dirname + '/lib/vendor');
 
+
 var sys                 = require('sys'),
     http                = require('http'),
     url                 = require('url'),
     qs                  = require('querystring'),
     MicroTemplate       = require('microtemplate/microtemplate'),
     MicroTemplateBundle = require('microtemplate/microtemplatebundle'),
-    
+
     templateDir         = __dirname + '/data/templates';
     rendererDir         = __dirname + '/data/renderers';
 
@@ -23,25 +24,22 @@ Object.map = function (obj, callback) {
   return arr;
 };
 
+
 http.createServer(function (req, res) {
 
-  var s = new Date();
-  
-  var query  = url.parse(req.url).query,
-      params = qs.parse(query),
+  var params = url.parse(qs.parse(query)).query,
       templates,
       bundle;
-  
+
   templates = Object.map(params, function (method, templateName) {
     return new MicroTemplate(method, templateName, templateDir, rendererDir);
   });
-  
+
   bundle = new MicroTemplateBundle(templates, rendererDir);
-  
+
   bundle.compile(function (err, compiledBundle) {
-    var comment = '/* Response time: ' + (new Date() - s) + 'ms */\n';
     res.writeHead(200, {'Content-Type': 'text/javascript'});
-    res.end(comment + compiledBundle);    
+    res.end(compiledBundle);
   });
 
 }).listen(8000);
